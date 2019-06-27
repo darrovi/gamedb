7573adf3d8058702c976ef82593e8c658fc7e96e
David Robles <david.robles@sngular.com>
2019-06-27 18:03:11 +0200

    Added firebase. Updated Gitignore.

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// noinspection ES6UnusedImports
+import firebaseApp from './firebase/init'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
+    router,
+    store,
+    render: function (h) {
+        return h(App)
+    }
 }).$mount('#app')
