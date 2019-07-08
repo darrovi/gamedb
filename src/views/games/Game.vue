74a0db2095fde694ce2e4d762d1aaa9fb0465767
David Robles <david.robles@sngular.com>
2019-07-08 11:58:08 +0200

    Description acordion

--- src/views/games/Game.vue
+++ src/views/games/Game.vue
@@ -15,37 +15,11 @@
         <div class="game__content" padding>
             <h2>Información</h2>
 
-            <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido
-                el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona
-                que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró
-                hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de
-                relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los
-                60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más
-                recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones
-                de Lorem Ipsum.
-
-                ¿Por qué lo usamos?
-                Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de
-                un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o
-                menos normal de las letras, al contrario de usar textos como por ejemplo "Contenido aquí, contenido
-                aquí". Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y
-                editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de "Lorem
-                Ipsum" va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de
-                desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras
-                veces a propósito (por ejemplo insertándole humor y cosas por el estilo).
-
-
-                ¿De dónde viene?
-                Al contrario del pensamiento popular, el texto de Lorem Ipsum no es simplemente texto aleatorio. Tiene
-                sus raices en una pieza cl´sica de la literatura del Latin, que data del año 45 antes de Cristo,
-                haciendo que este adquiera mas de 2000 años de antiguedad. Richard McClintock, un profesor de Latin de
-                la Universidad de Hampden-Sydney en Virginia, encontró una de las palabras más oscuras de la lengua del
-                latín, "consecteur", en un pasaje de Lorem Ipsum, y al seguir leyendo distintos textos del latín,
-                descubrió la fuente indudable. Lorem Ipsum viene de las secciones 1.10.32 y 1.10.33 de "de Finnibus
-                Bonorum et Malorum" (Los Extremos del Bien y El Mal) por Cicero, escrito en el año 45 antes de Cristo.
-                Este libro es un tratado de teoría de éticas, muy popular durante el Renacimiento. La primera linea del
-                Lorem Ipsum, "Lorem ipsum dolor sit amet..", viene de una linea en la sección 1.10.32
-
+            <p class="game__description" id="game-description">
+                {{game.description}}
+                <button @click="viewMoreDescription" class="game__more-description">
+                    <img src="@/assets/icons/more-arrow.svg">
+                </button>
             </p>
         </div>
 
@@ -79,6 +53,10 @@
                 } else {
                     imageEl.style.height = IMAGE_SIZE - SCROLL_CONTENT_MARGIN_TOP + 'px';
                 }
+            },
+            viewMoreDescription(event) {
+                document.getElementById('game-description').style.maxHeight = document.getElementById('game-description').scrollHeight + 'px';
+                event.target.remove();
             }
         }
     }
@@ -148,6 +126,24 @@
         &__content {
             // Tamaño de imagen menos tamaño del header
             margin-top: calc(136px + 62px);
+        }
+
+        &__description {
+            max-height: 78px;
+            overflow: hidden;
+            position: relative;
+            transition: max-height 0.3s ease-in-out;
+        }
+
+        &__more-description {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            left: 0;
+            margin: 0;
+            padding: 0;
+            background: $background-color;
+            opacity: 0.9;
         }
     }
 </style>
