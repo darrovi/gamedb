b108fcefd9aa3af72079a7dad2729705e7da41dc
David Robles <david.robles@sngular.com>
2019-07-10 20:53:11 +0200

    Removed list animation

--- src/views/games/Games.vue
+++ src/views/games/Games.vue
@@ -34,32 +34,9 @@
 <style scoped lang="scss">
     .games {
         &__list-item {
-            animation-name: fadeIn;
-            animation-duration: 0.3s;
-            animation-timing-function: ease-in-out;
-            animation-fill-mode: both;
-
             &:not(:last-child) {
                 margin-bottom: 16px;
             }
-
-            @for $i from 1 through 50 {
-                &:nth-child(#{$i}n) {
-                    animation-delay: #{$i * 0.1}s;
-                }
-            }
         }
     }
-
-    @keyframes fadeIn {
-        from {
-            opacity: 0;
-            transform: scale(0.5);
-        }
-        to {
-            opacity: 1;
-            transform: scale(1);
-        }
-    }
-
 </style>
