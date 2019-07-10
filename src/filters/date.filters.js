d45454baf4e2a3930390235ad8ef302de41a753b
David Robles <david.robles@sngular.com>
2019-07-10 10:58:37 +0200

    Fixed date filter

--- src/filters/date.filters.js
+++ src/filters/date.filters.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import moment from 'moment'
 
 Vue.filter('formatDate', function (value) {
-    if (value) {
+    const date = moment(String(value));
+    if (value && date.isValid()) {
         return moment(String(value)).format('DD/MM/YYYY')
+    } else {
+        return '---'
     }
 });
