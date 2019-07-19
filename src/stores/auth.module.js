103d4c884392b47238babd80e88bbd4110b6378f
David Robles <david.robles@sngular.com>
2019-07-19 13:22:55 +0200

    Added friends list

--- src/stores/auth.module.js
+++ src/stores/auth.module.js
@@ -22,16 +22,49 @@ const mutations = {
 
 const actions = {
     setUser({commit}, userId) {
-        db.collection('users').where('userId', '==', userId).get()
-            .then((snapshot) => {
-                snapshot.forEach((doc) => {
-                    this.user = {id: doc.id, ...doc.data()};
+        db.collection('users').where('userId', '==', userId).onSnapshot((snapshot) => {
+            snapshot.forEach((doc) => {
+                this.user = {id: doc.id, ...doc.data()};
+
+                if (this.user.friends && this.user.friends.length) {
                     firebase.storage().ref('users/' + this.user.imageRef).getDownloadURL().then((url) => {
                         this.user.image = url;
-                        commit('SET_USER', this.user)
+                    });
+
+                    const promises = [];
+
+                    for (const friend of this.user.friends) {
+                        promises.push(db.collection('users').doc(friend).get().catch());
+                    }
+
+                    Promise.all(promises).then((friendDocs) => {
+                        const friendsInfo = [];
+                        const imagesPromise = [];
+
+                        for (const friendDoc of friendDocs) {
+                            if (friendDoc && friendDoc.data()) {
+                                const friend = {id: friendDoc.id, ...friendDoc.data()};
+                                friendsInfo.push(friend);
+                                imagesPromise.push(firebase.storage().ref('users/' + friend.imageRef).getDownloadURL())
+                            }
+                        }
+                        this.user.friends = friendsInfo;
+
+                        Promise.all(imagesPromise).then((urls) => {
+                            for (let i = 0; i < friendsInfo.length; i++) {
+                                friendsInfo[i].image = urls[i]
+                            }
+                            commit('SET_USER', this.user);
+                        });
                     })
-                });
+                } else {
+                    firebase.storage().ref('users/' + this.user.imageRef).getDownloadURL().then((url) => {
+                        this.user.image = url;
+                        commit('SET_USER', this.user);
+                    });
+                }
             });
+        });
     },
     logout({commit}) {
         commit('LOGOUT')
--- src/views/user/Friends.vue
+++ src/views/user/Friends.vue
@@ -1,13 +1,110 @@
 <template>
-    <section>Friends</section>
+    <section padding has-header class="friends">
+        <header header>
+            <h1>{{$t('friends.title')}}</h1>
+
+            <img src="@/assets/icons/add.svg" @click="showAddFriendModal = true">
+        </header>
+
+        <ul v-if="user" class="friends__list">
+            <li v-for="friend of user.friends" :key="friend.id">
+                <img :src="friend.image">
+                <h2> {{friend.name}}</h2>
+            </li>
+        </ul>
+
+        <Modal v-show="showAddFriendModal">
+            <div slot="body">
+                <label for="friendId">{{$t('friends.friend-id')}}</label>
+                <input id="friendId" type="text" v-model="friendId"/>
+                <p class="friends__error" v-if="notFoundError">{{$t('friends.not-found')}}</p>
+            </div>
+
+            <template slot="buttons">
+                <button secondary @click="showAddFriendModal = false">{{$t('common.cancel')}}</button>
+                <button @click="addFriend()">{{$t('common.accept')}}</button>
+            </template>
+        </Modal>
+
+    </section>
 </template>
 
 <script>
+    import Modal from "../../components/Modal";
+    import {db} from '@/firebase/init';
+
     export default {
-        name: "Friends"
+        name: "Friends",
+        components: {Modal},
+        data() {
+            return {
+                showAddFriendModal: false,
+                friendId: '',
+                notFoundError: false
+            }
+        },
+        computed: {
+            user() {
+                return this.$store.getters['auth/user'];
+            }
+        },
+        methods: {
+            addFriend() {
+                let friends = this.user.friends || [];
+                friends.push(this.friendId);
+
+                db.collection('users').doc(this.friendId).get().then(
+                    (doc) => {
+                        if (doc.exists) {
+                            this.showAddFriendModal = false;
+                            db.collection('users').doc(this.user.id).update({
+                                friends: friends
+                            })
+                        } else {
+                            this.notFoundError = true;
+                        }
+                    }).catch(() => {
+                    this.notFoundError = true;
+                })
+            }
+        }
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+    .friends {
 
+        input {
+            margin-bottom: 0;
+        }
+
+        &__list {
+            margin: 0;
+            padding: 0;
+
+            li {
+                display: flex;
+                align-items: center;
+                padding: 16px 0;
+                border-bottom: 1px solid $card-color;
+
+                &:active {
+                    opacity: 0.8;
+                }
+
+                img {
+                    height: 50px;
+                    margin-right: 16px;
+                    border-radius: 50px;
+                }
+            }
+        }
+
+        &__error {
+            text-align: right;
+            color: $danger-color;
+            margin: 8px 0;
+            font-size: 14px;
+        }
+    }
 </style>
