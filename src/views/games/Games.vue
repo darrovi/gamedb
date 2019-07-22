a3127633d2879e4180aa537e25ae8c1f9d3e5800
David Robles <david.robles@sngular.com>
2019-07-22 12:57:09 +0200

    Added number of results

--- src/views/games/Games.vue
+++ src/views/games/Games.vue
@@ -15,7 +15,7 @@
         </header>
 
         <div class="games__has-filter" v-if="hasFilter">
-            <h2>{{$t('games.search-results')}}</h2>
+            <h2>{{games.length + ' ' + $tc('games.search-results', games.length)}}</h2>
             <button @click="clearFilters">{{$t('games.clear-filters')}}</button>
         </div>
 
